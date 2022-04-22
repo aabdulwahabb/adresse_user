49e4fb9dee9f9a7669643d222315219ac18485c7
aabdulwahabb <aa@versandmanufaktur.de>
2022-04-22 16:27:23 +0200

    abdul

--- resources/views/adresse/create.blade.php
+++ resources/views/adresse/create.blade.php
@@ -16,52 +16,71 @@
 @endif
 <form action="{{ url('/adresse') }}" method="POST" class="form-horizontal">
     {{ csrf_field() }}
+
     <div class="form-group">
-        <label for="formGroupExampleInput">Type:</label>
-        <input type="text" name="typ" id="typ" required class= "form-control" value="{{ old('typ') }}">
-        <small class="form-text text-muted">Type Ihr Konto: Frau oder Herr</small>
-    </div>
-    <div class="form-group">
-        <label for="formGroupExampleInput">Name:</label>
-        <input type="text" name="name" id="name" required class="form-control" value="{{ old('name') }}">
+    <select class="form-control" name="typ" id="typ">
+        <option class="form-control" value="{{ old('name') }}">Bitte wählen Sie Ihr Kontotype aus</option>
+        <option name="frau" value="frau">Frau</option>
+        <option name="herr" value="herr">Herr</option>
+    </select>
+        <small class="form-text text-muted">z.B. Frau oder Herr</small>
+        <input type="text" name="name" id="name" required placeholder="Ihr Name"
+               class="form-control" value="{{ old('name') }}">
         <small class="form-text text-muted">Vor und Nachname</small>
         @if ($errors->has('name'))
             <span class="error">{{ $errors->first('name') }}</span>
         @endif
-    </div>
-    <div class="form-group">
-        <label for="formGroupExampleInput">Email:</label>
-        <input type="email" name="email" id="email" required class="form-control" value="{{ old('email') }}">
+        <input type="email" name="email" id="email" required placeholder="Email Adresse"
+               class="form-control" value="{{ old('email') }}">
         <small class="form-text text-muted">Bitte gültige Email</small>
         @if ($errors->has('email'))
             <span class="error">{{ $errors->first('email') }}</span>
         @endif
     </div>
     <div class="form-group">
-        <label for="formGroupExampleInput">Project:</label>
-        <input type="text" name="projekt" id="projekt" required  class="form-control" value="{{ old('projekt') }}">
-        <small class="form-text text-muted">Entweder abkürzung: vm000 oder alle für alle projekte</small>
-        @if ($errors->has('projekt'))
-            <span class="error">{{ $errors->first('projekt') }}</span>
-        @endif
-    </div>
+        <label for="formGroupExampleInput">Bitte geben Sie Dienstleiter Type</label><br>
+        <input type="radio" id="freifeld1" name="freifeld1" value="{{ old('freifeld1') }}"
+               checked>
+        <label for="formGroupExampleInput">Intern</label>
+            <input type="radio" id="freifeld1" name="freifeld1" value="{{ old('freifeld1') }}"
+                   checked>
+            <label for="formGroupExampleInput">Extern</label>
+        </div>
     <div class="form-group">
         <label for="formGroupExampleInput">Username:</label>
         <input type="text" name="username" id="username" required class="form-control" value="{{ old('username') }}">
         @if ($errors->has('username'))
             <span class="error">{{ $errors->first('username') }}</span>
         @endif
-    </div>
-    <div class="form-group">
         <label for="formGroupExampleInput">Password:</label>
         <input type="password" name="password" id="password" required class="form-control" value="{{ old('password') }}">
         <small class="form-text text-muted">Mindestens 8 Zeichen</small>
         @if ($errors->has('password'))
             <span class="error">{{ $errors->first('password') }}</span>
         @endif
+   <label for="formGroupExampleInput">Team:</label>
+    <select class="form-control" name="typ" id="typ">
+        <option class="form-control">Bitte wählen Sie Ihr Team</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+        <label for="formGroupExampleInput">Telefon:</label>
+        <input type="text" name="telefon" id="telefon" placeholder="Optional"
+               class="form-control" value="{{ old('telefon') }}">
     </div>
     <div class="form-group">
-</div>
+        <label for="formGroupExampleInput">Ansprechpartner:</label>
+        <input type="text" name="ansprechpartner" id="ansprechpartner" placeholder="Optional"
+               class="form-control" value="{{ old('ansprechpartner') }}">
+    </div>
+    <div class="form-group">
+        <label for="formGroupExampleInput">Abteilung:</label>
+        <input type="text" name="abteilung" id="abteilung" placeholder="Optional"
+               class="form-control" value="{{ old('abteilung') }}">
+    </div>
+
+        <div class="form-group"></div>
 <!-- Add Button -->
  <div class="form-group">
      <div class="col-sm-offset-3 col-sm-6">
